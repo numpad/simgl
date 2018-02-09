571f535698742302cac1578a552718d3a709b25b
numpad <christian.schael87@gmail.com>
2018-02-09 14:25:51 +0000

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 
 #include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
-/*
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-*/
+
 #include "sgl_window.hpp"
 
 int main(int argc, char *argv[]) {
