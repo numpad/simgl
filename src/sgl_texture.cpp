e81c65b9bd88a08159c8d72e7a44357b1a3a9972
numpad <christian.schael87@gmail.com>
2018-03-02 13:06:02 +0000

    increased sgl::texture flexibility, now supporting cubemaps and many more GL_TEXTURE_* types

--- src/sgl_texture.cpp
+++ src/sgl_texture.cpp
@@ -4,19 +4,22 @@
 #include <stb_image.h>
 #undef STB_IMAGE_IMPLEMENTATION
 
-sgl::texture::texture()
-	: texture_id(0)
+sgl::texture::texture(GLenum texture_type)
+	: texture_id(0),
+	type(texture_type)
 {
 	glGenTextures(1, &this->texture_id);
 }
 
-sgl::texture::texture(std::string fname) : texture()
+sgl::texture::texture(std::string fname, GLenum texture_type)
+	: texture(texture_type)
 {
 	this->load(fname);
 }
 
 sgl::texture::texture(const sgl::texture &copy)
-	: texture_id(copy.texture_id)
+	: texture_id(copy.texture_id),
+	type(copy.type)
 {
 }
 
@@ -32,24 +35,70 @@ sgl::texture::operator GLuint() const
 
 bool sgl::texture::load(std::string fname)
 {
-	glBindTexture(GL_TEXTURE_2D, this->texture_id);
+	glBindTexture(this->type, this->texture_id);
 	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	/* texture parameters */
+	glTexParameteri(this->type, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(this->type, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(this->type, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(this->type, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	
-	//GLubyte *tex_data = SOIL_load_image(fname.c_str(), &this->_width, &this->_height, 0, SOIL_LOAD_RGBA);
-	int channels_in_file;
+	/* load texture data */
 	stbi_set_flip_vertically_on_load(1);
+	int channels_in_file;
 	unsigned char *tex_data = stbi_load(fname.c_str(), &this->_width, &this->_height, &channels_in_file, 0);
 	
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->_width, this->_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex_data);
-	glGenerateMipmap(GL_TEXTURE_2D);
+	/* upload texture data to gpu */
+	glTexImage2D(this->type, 0, GL_RGBA, this->_width, this->_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex_data);
+	glGenerateMipmap(this->type);
 	
+	/* free resources */
 	stbi_image_free(tex_data);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	
+	glBindTexture(this->type, 0);
+
 	return this->texture_id != 0;
+}
+
+bool sgl::texture::load_cubemap(std::vector<std::string> fnames)
+{
+	glBindTexture(this->type, this->texture_id);
+	
+	/* texture parameters */
+	glTexParameteri(this->type, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(this->type, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(this->type, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(this->type, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(this->type, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	
+	/*
+	 * load texture data
+	 * posx, negx, posy, negy, posz, negz
+	 */
+	stbi_set_flip_vertically_on_load(0);
+	GLint width, height, channels_count;
+	for (GLuint i = 0; i < fnames.size(); ++i) {
+		unsigned char *data = stbi_load(fnames[i].c_str(), &width, &height, &channels_count, 0);
+		glTexImage2D(
+			GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+			0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+		);
+
+		stbi_image_free(data);
+	}
+
+	glBindTexture(this->type, 0);
+
+	return this->texture_id != 0;
+}
+
+void sgl::texture::bind(GLint value)
+{
+	glActiveTexture(GL_TEXTURE0 + value);
+	glBindTexture(this->type, this->texture_id);
+}
+
+void sgl::texture::unbind()
+{
+	glBindTexture(this->type, 0);
 }
 
--- src/sgl_texture.hpp
+++ src/sgl_texture.hpp
@@ -2,26 +2,34 @@
 #define SGL_TEXTURE_HPP
 
 #include <string>
+#include <vector>
 #include <GL/gl3w.h>
 #include <stb_image.h>
 
 namespace sgl {
 
-class texture {
-	GLuint texture_id;
-	GLint _width, _height;
-	
-public:
-	
-	texture();
-	texture(std::string fname);
-	texture(const sgl::texture &copy);
-	~texture();
-	
-	bool load(std::string fname);
-	
-	operator GLuint() const;
-};
+	class texture {
+		GLuint texture_id;
+		GLint _width, _height;
+		
+		GLenum type;
+
+	public:
+		
+		texture(GLenum texture_type = GL_TEXTURE_2D);
+		texture(std::string fname, GLenum texture_type = GL_TEXTURE_2D);
+		
+		texture(const sgl::texture &copy);
+		~texture();
+		
+		bool load(std::string fname);
+		bool load_cubemap(std::vector<std::string> fnames);
+
+		operator GLuint() const;
+
+		void bind(GLint value);
+		void unbind();
+	};
 
 }
 
